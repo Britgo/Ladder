b29712c3e2004e334c5729370c162b38b4848eea
John M Collins <jmc@xisl.com>
2011-11-07 22:25:07 +0000

    Fix style.

--- php/head.php
+++ php/head.php
@@ -4,7 +4,7 @@
 </title>
 <meta name="generator" content="Bluefish 2.0.2" >
 <meta name="author" content="John M Collins" >
-<meta name="date" content="2011-11-07T21:17:59+0000" >
+<meta name="date" content="2011-11-07T22:24:48+0000" >
 <meta name="copyright" content="John Collins GPLv3">
 <meta name="keywords" content="Go,British Go Association,League">
 <meta name="description" content="League for BGA web pages">
@@ -12,5 +12,5 @@
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
 <meta http-equiv="content-style-type" content="text/css">
 <meta http-equiv="expires" content="0">
-<link href="/league/bgaladder-style.css" type="text/css" rel="stylesheet"></link>
+<link href="/bgaladder-style.css" type="text/css" rel="stylesheet"></link>
 </head>
