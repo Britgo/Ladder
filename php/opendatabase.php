f4a944025963658ac08ae019f867cb51b7475e12
John M Collins <jmc@toad.me.uk>
2017-01-22 15:42:51 +0000

    Use credentials routine for DB PW.

--- php/opendatabase.php
+++ php/opendatabase.php
@@ -1,6 +1,11 @@
 <?php
 
-//   Copyright 2009 John Collins
+//   Copyright 2009-2017 John Collins
+
+// *****************************************************************************
+// PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
+// Your changes may be lost or break things if you don't do it correctly!
+// *****************************************************************************
 
 //   This program is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
@@ -16,25 +21,36 @@
 //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-//  REMEMBER TO HACK THIS - Live requires username/password etc
-//  ***********************************************************
+include 'credentials.php';
 
-//  ALSO MAKE SURE that magic_quotes_gpc is turned off in your php init!!!!
-//  **********************************************************************
+try {
+	$dbcred = getcredentials('ladder');
+}
+catch (Credentials_error $e)  {
+	$mess = htmlspecialchars($e->getMessage());
+	print <<<EOT
+<html>
+<head>
+<title>Credentials error</title>
+<link href="/league/bgaleague-style.css" type="text/css" rel="stylesheet"></link>
+</head>
+<body>
+<h1>Credentials Error</h1>
+<p>Sorry but we were unable to fetch the credentials for the database - cannot proceed.
+Error message was: $mess.
+</p>
+<p>
+Please tell John Collins
+<a href="mailto:jmc@toad.me.uk">jmc@toad.me.uk</a> about this ASAP, and advise the context
+wherein it occurred. Thank you.</p>
+<p>Please now go to the top of the site by <a href="index.php">clicking here</a>.</p>
+</body>
+</html>
+EOT;
+	exit(0);
+}
 
-// Can't use "gethostname" function don't know why
-
-// $gh = popen('hostname', 'r');
-// $servername = fread($gh, 1024);
-// pclose($gh);
-// $servername = rtrim($servername);
-
-$dbserver = "localhost";
-$dbuser = "ladder-acc";
-$dbpassw = "bga@ladder@pw";
-$dbname = "ladder";
-
-if  (!mysql_connect($dbserver, $dbuser, $dbpassw)  ||  !mysql_select_db($dbname)) {
+if  (!mysql_connect("localhost", $dbcred->Username, $dbcred->Password)  ||  !mysql_select_db($dbcred->Databasename)) {
 
 	$mess = mysql_error();
 
